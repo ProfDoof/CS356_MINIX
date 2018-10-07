dd647165d52d30424eb8748182effbb480a35a6e
klemons <31633814+klemons@users.noreply.github.com>
2018-10-06 21:24:58 -0500

    Clean dmp.c file

--- servers/is/dmp.c
+++ servers/is/dmp.c
@@ -19,7 +19,7 @@ struct hook_entry {
 	{ F1, 	proctab_dmp, "Kernel process table" },
 	{ F2,   memmap_dmp, "Process memory maps" },
 	{ F3,	image_dmp, "System image" },
-	{ F4,	cs356_dmp, "Project Table" },
+	{ F4,	privileges_dmp, "Process privileges" },
 	{ F5,	monparams_dmp, "Boot monitor parameters" },
 	{ F6,	irqtab_dmp, "IRQ hooks and policies" },
 	{ F7,	kmessages_dmp, "Kernel messages" },
