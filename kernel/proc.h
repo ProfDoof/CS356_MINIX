c3e0acea3ce8caa016f51b7c6b8d2b5874ef1bea
U-DESKTOP-1RM2SBT\johns <jmm15f@acu.edu>
2018-10-07 20:07:40 -0500

    testing something

--- kernel/proc.h
+++ kernel/proc.h
@@ -20,9 +20,9 @@
 
 
 /* Will be size 1000 x 1000 */
-int os_cs356_proc_message_table[1000][1000];
-int os_cs356_proc_sum_sent[1000];
-int os_cs356_proc_sum_received[1000];
+EXTERN int os_cs356_proc_message_table[1000][1000];
+EXTERN int os_cs356_proc_sum_sent[1000];
+EXTERN int os_cs356_proc_sum_received[1000];
 
 #define CS356SENT(i) (&(os_cs356_proc_sum_sent[i]))
 #define CS356REC(i) (&(os_cs356_proc_sum_received[i]))
